b5a70d9c966c1e86866011996167a5c6ddebc4af
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2020-09-24 16:55:34 -0600

    successfully linked both JS files, updated questions and have successful return of info on newly created README file, from node app

--- Develop/index.js
+++ Develop/index.js
@@ -1,28 +1,77 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+var generateMd = require("./utils/generateMarkdown.js");
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is the title of the project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is the description of the project?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Who contributed on this project?",
+        name: "contributors"
+    },
+    {
+        type: "input",
+        message: "Project installation?",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "How do you use your application?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What tests were run while making this application?",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "Please provide email if user has any questions:",
+        name: "questions"
+    },
+    {
+        type: "input",
+        message: "Who is the author of this application?",
+        name: "author"
+    },
+    {
+        type: "list",
+        message: "Please choose a license from the following options:",
+        name: "license",
+        choices: ["MIT", "GNU GPLv3", "Apache 2.0"]
+    }
 ];
 
-var fs = require("fs");
 
-var generateMd = require("./utils/generateMarkdown.js");
-console.log(generateMd);
 
 // function to write README file
-fs.appendFile("README.md", process.argv[2] + '\n', function(err) {
-    if (err) {
-        return console.log(err);
-    }
-    else {
-        console.log("wrote to README");
-    }
+fs.writeFile("README.md", questions.response, "utf8", (error) => {
+    if (error) throw error;
+    console.log("Data appended to file!");
+
 });
 
-//function writeToFile(fileName, data) {
-//}
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions)
+    .then(function(data){
+        const markdown = generateMd(data);
+        fs.appendFile("README.md", markdown, (err) => {
+            if (err) throw err;
+            console.log("README written successfully");
+        });
+    })
 
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,46 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  ## Title
+  ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#install)
+  * [Usage](#usage)
+  * [Contributors](#contributors)
+  * [Test](#test)
+  * [Questions](#questions)
+  * [License](#license)
+  * [Author](#author)
+  
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Test
+  ${data.test}
+
+  ## Questions
+  ${data.questions}
+
+  ## License
+  ${data.license}
+
+  ## Author
+  ${data.author} 
 
 `;
 }
 
-var pie = "apple";
+
 
 module.exports = generateMarkdown;
